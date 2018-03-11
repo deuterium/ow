64b24e5d3ca09efcf2bfd59f1cf3b67099fceec9
Chris <chriswood.ca@gmail.com>
2018-03-11 15:50:19 -0700

    code for pulling windows update details. needs massassing to return objects to ow

--- ow-winclient/overwatch.ps1
+++ ow-winclient/overwatch.ps1
@@ -5,5 +5,17 @@ Import-Module $PSScriptRoot\ow-supportfunctions.psm1
 
 
 
+<#param(
+  [Parameter(Mandatory = $false,
+                    Position = 0,
+                    ValueFromPipelineByPropertyName = $true)]
+  [Boolean]$ShowPendingUpdates=$false,
+ 
+  [Parameter(Mandatory = $false,
+                    Position = 1,
+                    ValueFromPipelineByPropertyName = $true)]
+  [String]$LogFile=""
+)#>
+
 ToJSON (Get-FirewallStatus)
 #test
